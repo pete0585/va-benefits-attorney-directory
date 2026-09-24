89e2e08e10f43431fdb5afa1b884561c11248f65
Adam Peters <adam@thestrategicveteran.com>
2026-09-23 22:18:44 -0400

    feat(newsletter): add NewsletterSignup component to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import SearchBar from '@/components/SearchBar'
 import ListingCard from '@/components/ListingCard'
 import { getFeaturedListings, getTotalCount } from '@/lib/data'
 import { PRACTICE_AREAS, TOP_STATES, STATE_NAMES } from '@/lib/utils'
+import NewsletterSignup from '@/components/NewsletterSignup'
 
 export const metadata: Metadata = {
   title: 'Find VA-Accredited Attorneys | VA Disability Lawyer Directory',
